edbd7cf885a71e547854093da0f8fdec88e2b13d
Jason Bush <jasonbushis@gmail.com>
2012-04-01 22:02:53 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LendToFriend::Application.config.secret_token = '9c025b67dbd78a3019d783f5413a2494ab0d742c8a264d05c7e19a7dbbe3ba80a89d77609114e6ba76575f5dc2a3f7478301c6cc61e5e84caae9362497d994f6'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+LendToFriend::Application.routes.draw do
+  root :to => "home#index"
+end
